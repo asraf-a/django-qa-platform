51cd6e733b598294b79319da0706c296c113e1c5
Asraf A <asraf@testpress.in>
2026-09-18 12:16:12 +0530

    refactor: Extract BaseVoteView for question and answer voting
    ## Summary
    Extracts duplicated voting logic across question and answer voting views
    into a reusable base voting view.
    
    ## Changes
    - Created a shared `BaseVoteView` encapsulating vote parsing,
    upvote/downvote toggling, vote persistence, and redirection.
    - Refactored question voting to inherit from `BaseVoteView` with
    model-specific redirect logic.
    - Refactored answer voting to inherit from `BaseVoteView` while
    preserving anchor link redirection.
    - Removed duplicated voting methods and unused imports.

--- qa-platform/app/views/answer_views.py
+++ qa-platform/app/views/answer_views.py
@@ -1,15 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.contenttypes.models import ContentType
 from django.db.models import Prefetch
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.views import View
 from django.views.generic import CreateView, DeleteView, UpdateView
-from django.views.generic.detail import SingleObjectMixin
 
 from app.forms import AnswerForm
-from app.models import Answer, Question, Vote
-from .mixins import AuthorRequiredMixin
+from app.models import Answer, Question
+from .mixins import AuthorRequiredMixin, BaseVoteView
 
 
 class AnswerCreateView(LoginRequiredMixin, CreateView):
@@ -73,51 +70,10 @@ class AnswerDeleteView(LoginRequiredMixin, AuthorRequiredMixin, DeleteView):
         return reverse('app:question_detail', kwargs={'pk': self.object.question_id})
 
 
-class AnswerVoteView(LoginRequiredMixin, SingleObjectMixin, View):
+class AnswerVoteView(BaseVoteView):
     model = Answer
-
-    def post(self, request, *args, **kwargs):
-        answer = self.get_object()
-        vote_value = self._get_vote_value(request)
-        if vote_value is not None:
-            self._apply_vote(request.user, answer, vote_value)
-        return redirect(self._get_redirect_url(answer))
-
-    def get(self, request, *args, **kwargs):
-        answer = self.get_object()
-        return redirect(self._get_redirect_url(answer))
 
     def _get_redirect_url(self, answer):
         detail_url = reverse('app:question_detail', kwargs={'pk': answer.question_id})
         return f"{detail_url}#answer-{answer.pk}"
 
-    def _get_vote_value(self, request):
-        try:
-            value = int(request.POST.get('value', 0))
-            if value in (Vote.UPVOTE, Vote.DOWNVOTE):
-                return value
-        except (ValueError, TypeError):
-            pass
-        return None
-
-    def _apply_vote(self, user, answer, value):
-        content_type = ContentType.objects.get_for_model(Answer)
-        vote = Vote.objects.filter(
-            user=user,
-            content_type=content_type,
-            object_id=answer.pk
-        ).first()
-
-        if vote:
-            if vote.value == value:
-                vote.delete()
-            else:
-                vote.value = value
-                vote.save(update_fields=['value', 'updated_at'])
-        else:
-            Vote.objects.create(
-                user=user,
-                content_type=content_type,
-                object_id=answer.pk,
-                value=value
-            )
