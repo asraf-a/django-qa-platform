74bc417b0158188195bf828bd08786506038c68a
Asraf A <asraf@testpress.in>
2026-09-21 16:25:40 +0530

    feat: implement question sorting by newest, most voted, and unanswered

--- qa-platform/app/views/question_views.py
+++ qa-platform/app/views/question_views.py
@@ -1,14 +1,16 @@
 from urllib.parse import urlencode
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.messages.views import SuccessMessageMixin
-from django.db.models import Prefetch
+from django.db.models import IntegerField, OuterRef, Prefetch, Subquery, Sum, Value
+from django.db.models.functions import Coalesce
 from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
 from app.forms import AnswerCommentForm, AnswerForm, QuestionCommentForm, QuestionForm
-from app.models import Answer, Comment, Question, Tag
+from app.models import Answer, Comment, Question, Tag, Vote
 from .mixins import AuthorRequiredMixin, BaseVoteView, DeleteSuccessMessageMixin
 
 
@@ -27,11 +29,34 @@ class QuestionListView(ListView):
         tags = self.request.GET.getlist('tag')
         if tags:
             queryset = queryset.filter(tags__slug__in=tags).distinct()
+
+        sort = self.request.GET.get('sort', 'newest')
+        if sort == 'most_voted':
+            ct = ContentType.objects.get_for_model(Question)
+            vote_subquery = (
+                Vote.objects
+                .filter(content_type=ct, object_id=OuterRef('pk'))
+                .values('object_id')
+                .annotate(total=Sum('value'))
+                .values('total')
+            )
+            queryset = queryset.annotate(
+                vote_score=Coalesce(Subquery(vote_subquery, output_field=IntegerField()), Value(0))
+            ).order_by('-vote_score', '-created_at')
+        elif sort == 'unanswered':
+            queryset = queryset.filter(answers__isnull=True).order_by('-created_at')
+        else:
+            queryset = queryset.order_by('-created_at')
+
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         selected_slugs = self.request.GET.getlist('tag')
+        sort = self.request.GET.get('sort', 'newest')
+        if sort not in ('newest', 'most_voted', 'unanswered'):
+            sort = 'newest'
+
         all_tags = list(Tag.objects.all())
 
         for tag in all_tags:
@@ -40,15 +65,49 @@ class QuestionListView(ListView):
                 remaining = [s for s in selected_slugs if s != tag.slug]
             else:
                 remaining = selected_slugs + [tag.slug]
-            tag.toggle_url = (
-                f"?{urlencode({'tag': remaining}, doseq=True)}"
-                if remaining
-                else reverse('app:question_list')
-            )
+
+            params = {}
+            if remaining:
+                params['tag'] = remaining
+            if sort != 'newest':
+                params['sort'] = sort
+            query_str = urlencode(params, doseq=True)
+            tag.toggle_url = f"?{query_str}" if query_str else reverse('app:question_list')
+
+        # Generate sort tabs while preserving active tag filters
+        sort_options = [
+            ('newest', 'Newest'),
+            ('most_voted', 'Most Voted'),
+            ('unanswered', 'Unanswered'),
+        ]
+        sort_tabs = []
+        for sort_key, sort_label in sort_options:
+            params = {}
+            if selected_slugs:
+                params['tag'] = selected_slugs
+            if sort_key != 'newest':
+                params['sort'] = sort_key
+            query_str = urlencode(params, doseq=True)
+            sort_tabs.append({
+                'key': sort_key,
+                'label': sort_label,
+                'is_active': sort == sort_key,
+                'url': f"?{query_str}" if query_str else reverse('app:question_list'),
+            })
+
+        # URL for "All" tag reset that preserves current sort
+        all_tags_params = {'sort': sort} if sort != 'newest' else {}
+        context['all_tags_url'] = (
+            f"?{urlencode(all_tags_params)}"
+            if all_tags_params
+            else reverse('app:question_list')
+        )
 
         context['all_tags'] = all_tags
         context['selected_slugs'] = selected_slugs
         context['selected_tags'] = [t for t in all_tags if t.is_selected]
+        context['current_sort'] = sort
+        context['sort_tabs'] = sort_tabs
         return context
 
 
